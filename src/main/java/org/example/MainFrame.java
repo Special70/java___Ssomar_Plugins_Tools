c8fd5ab4ef5b1202d3ccb0c2c27c60fa4fb1eed5
Special70 <josef.urquico@gmail.com>
2024-07-24 14:24:42 +0800

    Uploading files before changing repository name

---
+++ src/main/java/org/example/MainFrame.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    MainFrame() {
+        this.setSize(1000, 800);
+        this.setTitle("Ssomar Plugins Tools");
+
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}

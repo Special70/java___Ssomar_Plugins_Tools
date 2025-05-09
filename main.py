390643874f45f5ec0b526a4db894cd07a59c84d3
Josef Miko Urquico <josef.urquico@gmail.com>
2025-05-09 20:34:37 +0800

    initial files

---
+++ main.py
@@ -0,0 +1,19 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200, 200, 300, 300)
+    win.setWindowTitle("Tech With Tim")
+
+    label = QtWidgets.QLabel(win)
+    label.setText("my first label")
+    label.move(50, 50)
+
+    win.show()
+    sys.exit(app.exec_())
+
+window()

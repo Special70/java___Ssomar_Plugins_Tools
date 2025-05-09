fa156ad16d033200d6574f891a4180fc6bd89b77
Josef Miko Urquico <josef.urquico@gmail.com>
2025-05-09 18:07:38 +0800

    wiped the files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,7 @@
 package org.example;
 
-import static org.example.System_JFrameObjectHandler.mainObj;
-
 public class Main {
-
-    /**
-     * For action trigger methods, go to {@link org.example.jframes} and its contents
-     */
     public static void main(String[] args) {
-        ResourceLoader resourceLoader = new ResourceLoader(); // Run resource loader. AKA setting up backend stuff such as settings
-        try {
-            resourceLoader.start();
-            resourceLoader.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        mainObj.swingUtilitiesInvokeLater(); // Load all GUI (Javax Swing) JFrames. AKA starting the actual program
+        System.out.println("Hello world!");
     }
-
 }

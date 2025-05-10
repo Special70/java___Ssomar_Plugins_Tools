44307238c11e479eda84671ec2291fca2f3f7fc0
Josef Miko Urquico <josef.urquico@gmail.com>
2025-05-10 20:14:23 +0800

    practicing javafx

--- src/main/java/org/example/ssomar_plugins_tools/HelloApplication.java
+++ src/main/java/org/example/ssomar_plugins_tools/HelloApplication.java
@@ -2,22 +2,40 @@ package org.example.ssomar_plugins_tools;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.BLUE); // like a JPanel/<div>
+
+        stage.setTitle("Stage Demo");
+        Image icon = new Image(getClass().getResource("/assets/images/icon.png").toExternalForm());
+        stage.getIcons().add(icon);
+        stage.setWidth(1280);
+        stage.setHeight(960);
+        stage.setResizable(false);
+
+
+
+
+        stage.setScene(scene);
+        // put this at the end of the code
+        stage.show();
     }
 }

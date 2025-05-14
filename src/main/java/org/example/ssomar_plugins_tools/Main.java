a0811b54dee030d28decc295d0661f5f38077c9f
Josef Miko Urquico <josef.urquico@gmail.com>
2025-05-14 13:13:58 +0800

    updated code
    - added some pom.xml writing
    - use java 24

--- src/main/java/org/example/ssomar_plugins_tools/Main.java
+++ src/main/java/org/example/ssomar_plugins_tools/Main.java
@@ -1,17 +1,18 @@
 package org.example.ssomar_plugins_tools;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.application.Platform;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import org.example.ssomar_plugins_tools.class_assets.LibClass;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
 
 
     public static void main(String[] args) {
@@ -20,22 +21,22 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        LibClass libclass = new LibClass();
 
-        Group root = new Group();
+        // assets
+        Image icon = new Image(libclass.applicationIcon);
+
+        // scenes
         Scene scene = new Scene(root, Color.BLUE); // like a JPanel/<div>
 
+        // stages
         stage.setTitle("Stage Demo");
-        Image icon = new Image(getClass().getResource("/assets/images/icon.png").toExternalForm());
         stage.getIcons().add(icon);
-        stage.setWidth(1280);
-        stage.setHeight(960);
-        stage.setResizable(false);
-
-
-
 
         stage.setScene(scene);
-        // put this at the end of the code
+
+
+        Platform.runLater(stage::centerOnScreen);
         stage.show();
     }
 }
